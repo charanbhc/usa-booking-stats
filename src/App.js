97591f97fbe0ca9bf71d4a2918db04d17135e957
Charan <charanbhc@Charans-MacBook-Air.local>
2025-07-11 14:27:57 +0530

    .

--- src/App.js
+++ src/App.js
@@ -145,7 +145,10 @@ function App() {
 
   return (
     <div className="App">
-      <h1>🎟️ HHVM USA Bookings @TeluguHarry</h1>
+      <h1>🎟️ HHVM USA Bookings   <a href="https://twitter.com/TeluguHarry" target="_blank" rel="noopener noreferrer">
+    @TeluguHarry
+  </a>
+</h1>
 
       <div className="dropdown-container">
         <label htmlFor="date-select">Select Date: </label>
